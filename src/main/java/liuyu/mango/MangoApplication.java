ce4a36a389972099bdf7368f376daa090e412223
liuyu2012 <liuyu2012@yeah.net>
2019-12-05 21:07:40 +0800

    web页面访问echo

--- src/main/java/liuyu/mango/MangoApplication.java
+++ src/main/java/liuyu/mango/MangoApplication.java
@@ -2,8 +2,13 @@ package liuyu.mango;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * @author Administrator
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class MangoApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/liuyu/mango/com/web/WebController.java
@@ -0,0 +1,21 @@
+package liuyu.mango.com.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 访问控制器
+ *
+ * @author Administrator
+ */
+@RestController
+@RequestMapping("/query")
+public class WebController {
+
+    @GetMapping("/echo")
+    public String echo() {
+        return "第一次访问数据";
+    }
+
+}
